14a2cae9295567d82b841180ce82973323989d15
Simon Kendall <simon.a.kendall@gmail.com>
2024-05-21 16:08:11 +0200

    add webhook payload dto

--- src/main/java/eu/kendall/simon/mobhub/mobhubv1/controllers/WebhookController.java
+++ src/main/java/eu/kendall/simon/mobhub/mobhubv1/controllers/WebhookController.java
@@ -1,5 +1,6 @@
 package eu.kendall.simon.mobhub.mobhubv1.controllers;
 
+import eu.kendall.simon.mobhub.mobhubv1.dtos.WebhookPayloadDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,8 +9,8 @@ public class WebhookController {
 
     @PostMapping(value = "/push", consumes = "application/json")
     @ResponseBody
-    public ResponseEntity<String> handlePush(@RequestBody(required = false) Object requestBody) {
-        System.out.println("Request received.");
+    public ResponseEntity<String> handlePush(@RequestBody(required = false) WebhookPayloadDTO requestBody) {
+        System.out.println("Request received. Zen is "+requestBody.zen());
 
         return ResponseEntity.ok().body("Thanks");
     }
---
+++ src/main/java/eu/kendall/simon/mobhub/mobhubv1/dtos/WebhookPayloadDTO.java
@@ -0,0 +1,6 @@
+package eu.kendall.simon.mobhub.mobhubv1.dtos;
+
+public record WebhookPayloadDTO(
+        String zen
+) {
+}
