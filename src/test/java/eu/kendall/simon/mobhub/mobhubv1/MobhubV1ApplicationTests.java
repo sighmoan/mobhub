543fcc253245bddcf6a61b92782528eadab7f4b6
Simon Kendall <simon.a.kendall@gmail.com>
2024-05-21 15:52:48 +0200

    set up mockmvc to test controller

--- src/test/java/eu/kendall/simon/mobhub/mobhubv1/MobhubV1ApplicationTests.java
+++ src/test/java/eu/kendall/simon/mobhub/mobhubv1/MobhubV1ApplicationTests.java
@@ -1,13 +1,34 @@
 package eu.kendall.simon.mobhub.mobhubv1;
 
+import eu.kendall.simon.mobhub.mobhubv1.controllers.WebhookController;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class MobhubV1ApplicationTests {
 
+	MockMvc mockmvc;
+
+	@BeforeEach
+	void setup() {
+		this.mockmvc = MockMvcBuilders.standaloneSetup(new WebhookController())
+				.alwaysExpect(status().isOk())
+				.build();
+	}
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void controllerResponds() throws Exception {
+		this.mockmvc.perform(post("/push"));
 	}
 
 }
