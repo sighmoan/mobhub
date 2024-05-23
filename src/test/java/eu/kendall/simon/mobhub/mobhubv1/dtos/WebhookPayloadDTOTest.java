f21f232ea7e35ab957ac42306802400ba1dcc248
Simon Kendall <simon.a.kendall@gmail.com>
2024-05-23 16:38:48 +0200

    use jackson object mapper in tests to load gh sample response

--- src/test/java/eu/kendall/simon/mobhub/mobhubv1/dtos/WebhookPayloadDTOTest.java
+++ src/test/java/eu/kendall/simon/mobhub/mobhubv1/dtos/WebhookPayloadDTOTest.java
@@ -1,7 +1,10 @@
 package eu.kendall.simon.mobhub.mobhubv1.dtos;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.kendall.simon.mobhub.mobhubv1.models.WebhookEvent;
 import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,7 +13,22 @@ class WebhookPayloadDTOTest {
     @Test
     void shouldReturnWebhookEventOnRequest() {
         // Arrange
-        WebhookPayloadDTO dto = new WebhookPayloadDTO("zen", "hookName", "hookType");
+        ObjectMapper objMapper = new ObjectMapper();
+        WebhookPayloadDTO dto = null;
+        byte[] ghResponseJson = null;
+        try {
+            InputStream str = getClass().getClassLoader()
+                    .getResourceAsStream("static/sample-gh-response.json");
+            if(str == null) fail("Unable to open stream to gh response");
+            ghResponseJson = str.readAllBytes();
+        } catch(Exception e) {
+            fail("Unable to load sample gh response into byte array.");
+        }
+        try {
+            dto = objMapper.readValue(ghResponseJson, WebhookPayloadDTO.class);
+        } catch(Exception e) {
+            fail("Unable to turn sample gh response into dto" + e.getMessage());
+        }
         // Act
         WebhookEvent event = dto.getEvent();
         // Assert
