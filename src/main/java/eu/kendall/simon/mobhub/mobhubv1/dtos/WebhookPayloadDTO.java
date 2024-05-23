67ffa35356cbce075117a3254b9c1f98aa09caae
Simon Kendall <simon.a.kendall@gmail.com>
2024-05-23 16:41:32 +0200

    construct users in webhook payload dto

--- src/main/java/eu/kendall/simon/mobhub/mobhubv1/dtos/WebhookPayloadDTO.java
+++ src/main/java/eu/kendall/simon/mobhub/mobhubv1/dtos/WebhookPayloadDTO.java
@@ -1,20 +1,20 @@
 package eu.kendall.simon.mobhub.mobhubv1.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import eu.kendall.simon.mobhub.mobhubv1.models.User;
 import eu.kendall.simon.mobhub.mobhubv1.models.WebhookEvent;
 
 import java.util.Map;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class WebhookPayloadDTO {
     public String zen;
     public String hookName;
     public String hookType;
-
-    public WebhookPayloadDTO(String zen, String hookName, String hookType) {
-        this.zen = zen;
-        this.hookName = hookName;
-        this.hookType = hookType;
-    }
+    public String senderId;
+    public String senderName;
+    public String senderAvatarUrl;
 
     @SuppressWarnings("unchecked")
     @JsonProperty("hook")
@@ -23,8 +23,16 @@ public class WebhookPayloadDTO {
         hookType = hookData.get("type").toString();
     }
 
+    @JsonProperty("sender")
+    public void mapSender(Map<String, Object> userData) {
+        senderId = userData.get("id").toString();
+        senderName = userData.get("login").toString();
+        senderAvatarUrl = userData.get("avatar_url").toString();
+    }
+
     public WebhookEvent getEvent() {
         WebhookEvent event = null;
+        User user = new User(senderName, senderAvatarUrl, senderId);
         return event;
     }
 }
--- src/main/java/eu/kendall/simon/mobhub/mobhubv1/models/User.java
+++ src/main/java/eu/kendall/simon/mobhub/mobhubv1/models/User.java
@@ -3,4 +3,11 @@ package eu.kendall.simon.mobhub.mobhubv1.models;
 public class User {
     String displayName;
     String avatarUrl;
+    String userId;
+
+    public User(String displayName, String avatarUrl, String userId) {
+        this.displayName = displayName;
+        this.avatarUrl = avatarUrl;
+        this.userId = userId;
+    }
 }
